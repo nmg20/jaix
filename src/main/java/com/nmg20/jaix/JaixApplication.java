6bfec07b8e8abb226cdabb413c55b88b1a28267b
nmg20 <nicogm977@gmail.com>
2025-11-24 19:22:15 +0100

    Initial base setup

---
+++ src/main/java/com/nmg20/jaix/JaixApplication.java
@@ -0,0 +1,13 @@
+package com.nmg20.jaix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nmg20/jaix/JaixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmg20.jaix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
